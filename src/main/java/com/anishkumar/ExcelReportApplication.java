8d6087cfc7edf18b763c785ad7027bbd070e315b
anishkumar <gsranish@gmail.com>
2024-10-17 14:52:33 +0530

    Initial code was added for integration

--- src/main/java/com/anishkumar/ExcelReportApplication.java
+++ src/main/java/com/anishkumar/ExcelReportApplication.java
@@ -1,12 +1,16 @@
 package com.anishkumar;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ExcelReportApplication {
 
 	public static void main(String[] args) {
+		log.debug("Excel Report Application started at DEBUG MODE ");
 		SpringApplication.run(ExcelReportApplication.class, args);
 	}
 
--- src/main/java/com/anishkumar/controller/ExcelReportController.java
+++ src/main/java/com/anishkumar/controller/ExcelReportController.java
@@ -3,28 +3,28 @@ package com.anishkumar.controller;
 import com.anishkumar.service.ExcelReportService;
 import jakarta.servlet.http.HttpServletResponse;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class ExcelReportController {
 
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(ExcelReportController.class);
     @Autowired
     private ExcelReportService reportService;
 
     @GetMapping("/excel")
-    public void generateExcelReport(HttpServletResponse response) throws Exception{
-
+    public void generateExcelReport(HttpServletResponse response) throws IOException {
+        LOGGER.info("ExcelReportController : generateExcelReport called ");
         response.setContentType("application/octet-stream");
-
         String headerKey = "Content-Disposition";
         String headerValue = "attachment;filename=courses.xls";
-
         response.setHeader(headerKey, headerValue);
-
         reportService.generate(response);
-
         response.flushBuffer();
     }
 
